d628dd5f9d2937edf796f6abae697287f294a920
iKlem <iklem.d@gmail.com>
2014-10-19 20:09:30 +0200

    Added files + lua

--- fastdl.lua
+++
@@ -1,3 +0,0 @@
-resource.AddFile("models/player/anon/anon.mdl")
-resource.AddFile("materials/models/player/anon/body_d.vmt")
-resource.AddFile("materials/models/player/anon/body_d.vtf")
---
+++ lua/autorun/server/fastdl.lua
@@ -0,0 +1,36 @@
+-- BOXMAN MODEL PLAYER 
+--resource.AddFile("models/vinrax/player/boxman_player.mdl")
+
+-- POKEMON RED MODEL PLAYER
+resource.AddFile("models/player/red/red.mdl")
+resource.AddFile("materials/models/pokemon/flat_normal.vmt")
+resource.AddFile("materials/models/pokemon/red_body.vmt")
+resource.AddFile("materials/models/pokemon/red_body.vtf")
+resource.AddFile("materials/models/pokemon/red_body_player.vmt")
+resource.AddFile("materials/models/pokemon/red_body_player.vtf")
+resource.AddFile("materials/models/pokemon/red_eye.vmt")
+resource.AddFile("materials/models/pokemon/red_eye.vtf")
+resource.AddFile("materials/models/pokemon/red_eye_player.vmt")
+resource.AddFile("materials/models/pokemon/red_eye_player.vtf")
+
+-- JACK SKELLINGTON MODEL PLAYER
+--resource.AddFile("models/vinrax/player/jack_player.mdl")
+--resource.AddFile("materials/models/vinrax/jack_skellington/cloth.vmt")
+--resource.AddFile("materials/models/vinrax/jack_skellington/cloth.vtf")
+--resource.AddFile("materials/models/vinrax/jack_skellington/skull.vmt")
+--resource.AddFile("materials/models/vinrax/jack_skellington/skull.vtf")
+
+-- TRON ANON MODEL PLAYER
+resource.AddFile("models/player/anon/anon.mdl")
+resource.AddFile("materials/models/player/anon/baton_d.vmt")
+resource.AddFile("materials/models/player/anon/baton_d.vtf")
+resource.AddFile("materials/models/player/anon/baton_n.vtf")
+resource.AddFile("materials/models/player/anon/body_d.vmt")
+resource.AddFile("materials/models/player/anon/body_d.vtf")
+resource.AddFile("materials/models/player/anon/body_n.vtf")
+resource.AddFile("materials/models/player/anon/disc_d.vmt")
+resource.AddFile("materials/models/player/anon/disc_d.vtf")
+resource.AddFile("materials/models/player/anon/disc_n.vtf")
+resource.AddFile("materials/models/player/anon/helmet_d.vmt")
+resource.AddFile("materials/models/player/anon/helmet_d.vtf")
+resource.AddFile("materials/models/player/anon/helmet_n.vtf")
